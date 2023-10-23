1f972a53397403acc16fa19afc4f91a2de5fdbe5
I-luk-I <105889683+I-luk-I@users.noreply.github.com>
2023-10-23 22:12:53 +0300

    Update system solution.py

--- system solution.py
+++ system solution.py
@@ -10,10 +10,7 @@ def solution (syst:str)->tuple:
     if multiplier_matrix[1][0]!=0:
         divider_1=multiplier_matrix[1][0]
         for i in range(len(multiplier_matrix[1])):
-            if divider_1>0:
-                multiplier_matrix[1][i]=multiplier_matrix[0][i]*(-divider_1)+multiplier_matrix[1][i]
-            elif divider_1<0:
-                multiplier_matrix[1][i]=multiplier_matrix[0][i]*(-divider_1)+multiplier_matrix[1][i]
+           multiplier_matrix[1][i]=multiplier_matrix[0][i]*(-divider_1)+multiplier_matrix[1][i]
     y=multiplier_matrix[1][-1]/multiplier_matrix[1][1]
     if multiplier_matrix[0][1]*y>0:
         x=multiplier_matrix[0][2]-(multiplier_matrix[0][1]*y)
