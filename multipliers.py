41996344ccd5d3a5d4c432e5ff88eea3fb3c945e
I-luk-I <105889683+I-luk-I@users.noreply.github.com>
2023-12-25 23:06:16 +0300

    Update multipliers.py

--- multipliers.py
+++ multipliers.py
@@ -1,30 +1,30 @@
 def matrix_init (linear_equation):
-    matrix=[]
-    transform=linear_equation.split(' ')
-    unknown_x=[]
-    unknown_y=[]
-    right_part=[]
+    matrix = []
+    transform = linear_equation.split(' ')
+    unknown_x = []
+    unknown_y = []
+    right_part = []
     for i in range (len(transform)):
         if 'x' in transform[i]:
-            if transform[i-1]=='-'and transform[i][0]=='x':
+            if transform[i-1] == '-'and transform[i][0] == 'x':
                 unknown_x.append(-1)
-            elif transform[i][0]=='x':
+            elif transform[i][0] == 'x':
                 unknown_x.append(1)
-            elif transform[i-1]=='-':
+            elif transform[i-1] == '-':
                 unknown_x.append(-(int(transform[i][:-1])))
             else:
                 unknown_x.append(int(transform[i][:-1]))
                     
         elif 'y' in transform[i]:
-            if transform[i-1]=='-' and transform[i][0]=='y':
+            if transform[i-1] == '-' and transform[i][0] == 'y':
                 unknown_y.append(-1)
-            elif transform[i][0]=='y':
+            elif transform[i][0] == 'y':
                 unknown_y.append(1)
-            elif transform[i-1]=='-':
+            elif transform[i-1] == '-':
                 unknown_y.append(-(int(transform[i][:-1])))   
             else:
                 unknown_y.append(int(transform[i][:-1]))
-        elif  transform[i]=='=':
+        elif  transform[i] == '=':
             right_part.append(int(transform[i+1]))
             
     for i in range(len(unknown_x)):
